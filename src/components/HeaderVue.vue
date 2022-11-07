b50ca800f533e5f0987220ea6cef58fc6d38aa13
Falain Zeukang <ramfalain@gmail.com>
2022-11-07 15:11:25 +0100

    component addition

---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ButtonVue text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+
+import ButtonVue from './ButtonVue.vue';
+
+export default {
+   name: 'HeaderVue',
+   props: {
+    title: String,
+   
+   },
+   components: {
+    ButtonVue
+
+}
+
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    
+</style>
---
+++ src/components/TasksVue.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <TaskVue @delete-task="$emit('delete-task', task.id)" :task="task"/>
+
+    </div>
+    
+</template>
+<script>
+import TaskVue from './TaskVue.vue';
+
+export default{
+    name: 'TasksVue',
+
+    props: {
+        tasks: Array,
+    },
+    
+    components : {
+        TaskVue
+    },
+    emits:['delete-task'],
+
+}
+</script>
